GravityKit/Dashboard-Views

commit 65cc2706e939f5fd5d86d928113518967e7bc684

Cache function responses to improve performance

src/FoundationSettings.php
@@ -31,6 +31,8 @@ class FoundationSettings {
 	 * @return array The modified settings object.
 	 */
 	public function add_global_gravityview_settings( $settings ) {
+		static $dashboard_views_settings;
+
 		if ( empty( $settings['gravityview']['sections'] ) || ! class_exists( 'GravityKitFoundation' ) ) {
 			return $settings;
 		}
@@ -38,6 +40,15 @@ class FoundationSettings {
 		$gravityview_settings_id = GravityViewPluginSettings::SETTINGS_PLUGIN_ID;
 		$gravityview_settings    = GravityKitFoundation::settings()->get_plugin_settings( $gravityview_settings_id );
 		$site_url                = is_multisite() ? network_home_url() : home_url();
+
+		if ( $dashboard_views_settings ) {
+			$settings[ $gravityview_settings_id ]['sections'][] = [
+				'title'    => esc_html__( 'Dashboard Views', 'gk-gravityview-dashboard-views' ),
+				'settings' => $dashboard_views_settings,
+			];
+
+			return $settings;
+		}
 
 		$supported_css_frameworks = [
 			'chota'  => [

src/AdminMenu.php
@@ -5,6 +5,7 @@ namespace GravityKit\GravityView\DashboardViews;
 use GravityKit\GravityView\Foundation\WP\AdminMenu as FoundationAdminMenu;
 use GravityKitFoundation;
 use GV\Plugin_Settings as GravityViewPluginSettings;
+
 /**
  * This class configures the Admin (Dashboard) menu.
  *
@@ -279,6 +280,12 @@ class AdminMenu extends FoundationAdminMenu {
 	 * @return string
 	 */
 	public static function get_view_submenu_prefix() {
+		static $prefix;
+
+		if ( $prefix ) {
+			return $prefix;
+		}
+
 		$gravityview_settings = GravityKitFoundation::settings()->get_plugin_settings( GravityViewPluginSettings::SETTINGS_PLUGIN_ID );
 
 		$prefix = sanitize_title( $gravityview_settings['dashboard_views_menu_name'] ?? esc_html__( 'Dashboard Views', 'gk-gravityview-dashboard-views' ) );

commit e460d5bae9879f6e34405d732cb210cc0547d7d1

Fix Foundation settings that broke in 65cc270

src/FoundationSettings.php
@@ -31,8 +31,6 @@ class FoundationSettings {
 	 * @return array The modified settings object.
 	 */
 	public function add_global_gravityview_settings( $settings ) {
-		static $dashboard_views_settings;
-
 		if ( empty( $settings['gravityview']['sections'] ) || ! class_exists( 'GravityKitFoundation' ) ) {
 			return $settings;
 		}
@@ -40,15 +38,6 @@ class FoundationSettings {
 		$gravityview_settings_id = GravityViewPluginSettings::SETTINGS_PLUGIN_ID;
 		$gravityview_settings    = GravityKitFoundation::settings()->get_plugin_settings( $gravityview_settings_id );
 		$site_url                = is_multisite() ? network_home_url() : home_url();
-
-		if ( $dashboard_views_settings ) {
-			$settings[ $gravityview_settings_id ]['sections'][] = [
-				'title'    => esc_html__( 'Dashboard Views', 'gk-gravityview-dashboard-views' ),
-				'settings' => $dashboard_views_settings,
-			];
-
-			return $settings;
-		}
 
 		$supported_css_frameworks = [
 			'chota'  => [