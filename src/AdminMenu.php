88ef15840eeee4b53b93e938a430b34db4bdbec4
Vlad <moi@mrcasual.com>
2024-05-30 08:23:13 -0400

    Update filter name to use the our naming convention [ci skip]

--- src/AdminMenu.php
+++ src/AdminMenu.php
@@ -131,7 +131,7 @@ class AdminMenu extends FoundationAdminMenu {
 		/**
 		 * Controls the icon URL of the top-level Dashboard Views menu.
 		 *
-		 * @filter 'gk/gravityview/dashboard-views/admin-menu/icon_url'
+		 * @filter 'gk/gravityview/dashboard-views/admin-menu/icon-url'
 		 *
 		 * @since  TBD
 		 *
@@ -143,7 +143,7 @@ class AdminMenu extends FoundationAdminMenu {
 		 *                              * Pass 'none' to leave div.wp-menu-image empty so an icon can be added via CSS.
 		 */
 		$icon_url = apply_filters(
-			'gk/gravityview/dashboard-views/admin-menu/icon_url',
+			'gk/gravityview/dashboard-views/admin-menu/icon-url',
 			'data:image/svg+xml;base64,' . base64_encode( '<svg id="Artwork" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="#a7aaad" class="st0" d="M128 0C57.3 0 0 57.3 0 128s57.3 128 128 128 128-57.3 128-128S198.7 0 128 0zm0 243.2c-63.6 0-115.2-51.6-115.2-115.2S64.4 12.8 128 12.8 243.2 64.4 243.2 128 191.6 243.2 128 243.2zm7.9-172.5c-.8.1-1.4-.5-1.5-1.3V57.7c-.1-.9.4-1.8 1.3-2.1 7.8-4.2 10.6-13.9 6.4-21.7-4.2-7.8-13.9-10.6-21.7-6.4-7.8 4.2-10.6 13.9-6.4 21.7 1.5 2.7 3.7 4.9 6.4 6.4.8.3 1.4 1.2 1.3 2.1v11.4c.1.8-.4 1.5-1.2 1.6h-.3c-41 3-68.9 29.6-68.9 66.9 0 39.6 31.5 67.2 76.8 67.2s76.8-27.6 76.8-67.2c-.1-37.3-28-63.9-69-66.9zM128 182.4c-35.9 0-60.8-18.4-60.8-44.8S92.1 92.8 128 92.8s60.8 18.4 60.8 44.8-24.9 44.8-60.8 44.8zm53.8-44.8c0 22.3-22.1 37.8-53.8 37.8-5.1 0-10.2-.4-15.2-1.3-6.8-1.2-9.4-3.2-12-9.6-3.1-7.5-4.8-16.6-4.8-26.9s1.7-19.4 4.8-26.9c2.7-6.4 5.2-8.4 12-9.6 5-.9 10.1-1.3 15.2-1.3 31.7 0 53.8 15.5 53.8 37.8z"/></svg>' ) // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.obfuscation_base64_encode
 		);
 
